3a3ae9499420d2746621afbe1e6a535ee5329140
Art Eschenlauer <esch0041@umn.edu>
2020-02-23 13:45:37 -0600

    refactored setup of registry value

--- install_example.cmd
+++ install_example.cmd
@@ -1,16 +1,42 @@
 @echo off
+
+if /I "%1" == "help" goto :usage
+if /I "%1" == "--help" goto :usage
+if /I "%1" == "-h" goto :usage
+if /I "%1" == "-?" goto :usage
+if /I "%1" == "/?" goto :usage
+if /I "%1" == "-?" goto :usage
+
 setlocal
-set MY_LOG4CMD_ROOT_EX=%USERPROFILE%\AppData\Local\log4cmd
-set MY_LOG4CMD_ROOT=%%USERPROFILE%%\AppData\Local\log4cmd
+if exist "%~dp0\log4cmd_regkey.cmd" call "%~dp0\log4cmd_regkey.cmd"
+if "%LOG4CMD_REGKEYVAL%" == "" call "%~dp0\log4cmd_regkey_example.cmd"
+if /I "%1" == "remove" goto :remove
 
-call "%~dp0\log4cmd_regkey.cmd"
-
-if     exist "%MY_LOG4CMD_ROOT_EX%" echo Found directory %MY_LOG4CMD_ROOT_EX%
-if not exist "%MY_LOG4CMD_ROOT_EX%" echo Attempting to create directory %MY_LOG4CMD_ROOT_EX%
-echo Installing registry value "%LOG4CMD_REGVAL%" under key "%LOG4CMD_REGKEY%"
+echo Set registry value "%LOG4CMD_REGVAL%" under key "%LOG4CMD_REGKEY%" to "%LOG4CMD_ROOT%"?
+if not exist "%LOG4CMD_ROOT_EX%" echo Create directory "%LOG4CMD_ROOT%"?
+echo If you do not wish to proceeed, press control-C and then type Y to terminate this script.
+:: pause to get confirmation before proceeding
 pause
-if not exist "%MY_LOG4CMD_ROOT_EX%" mkdir "%MY_LOG4CMD_ROOT_EX%"
-reg add   "%LOG4CMD_REGKEY%" /f /t REG_EXPAND_SZ /d %MY_LOG4CMD_ROOT% /v "%LOG4CMD_REGVAL%"
+if     exist "%LOG4CMD_ROOT_EX%" echo Found directory %LOG4CMD_ROOT_EX%
+if not exist "%LOG4CMD_ROOT_EX%" echo Attempting to create directory %LOG4CMD_ROOT_EX%
+if not exist "%LOG4CMD_ROOT_EX%" mkdir "%LOG4CMD_ROOT_EX%"
+reg add   "%LOG4CMD_REGKEY%" /f /t REG_EXPAND_SZ /d %LOG4CMD_ROOT% /v "%LOG4CMD_REGVAL%"
 reg query "%LOG4CMD_REGKEY%" /v "%LOG4CMD_REGVAL%"
-
 endlocal
+goto :eof
+
+:remove
+
+echo Deleting registry value "%LOG4CMD_REGVAL%" under key "%LOG4CMD_REGKEY%"
+:: reg delete will prompt before acting, so no need for confirmation before proceeding
+reg delete "%LOG4CMD_REGKEY%" /v "%LOG4CMD_REGVAL%"
+endlocal
+goto :eof
+
+:usage
+
+echo . To install registry value for LOG4CMD: %0
+echo . To remove registry value for LOG4CMD: %0 remove
+echo . To configure which values in the registry are used:
+echo .   copy log4cmd_regkey_example.cmd to log4cmd_regkey.cmd
+echo .   and customize the latter before running %0
--- log4cmd_regkey_example.cmd
+++ log4cmd_regkey_example.cmd
@@ -1,3 +1,6 @@
 @set LOG4CMD_REGKEY=HKCU\Environment
 @set LOG4CMD_REGVAL=log4cmd
 @set LOG4CMD_REGKEYVAL=HKCU\Environment\log4cmd
+@set LOG4CMD_ROOT_EX=%USERPROFILE%\AppData\Local\log4cmd
+@set LOG4CMD_ROOT=%%USERPROFILE%%\AppData\Local\log4cmd
+
